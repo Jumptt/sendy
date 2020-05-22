bfe753525ad0805944c13e5211052edba4de9ab9
Jumptt <64533765+Jumptt@users.noreply.github.com>
2020-05-22 15:46:30 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>send</title>
+    <link rel="stylesheet" href="query.css">
+  </head>
+  <body>
+    <form class="" action="query.php" method="post">
+      <h1>お問い合わせ内容</h1> 
+      <p>To <span>*</span> </p> <input type="text" name="to" value="">
+      <p>Title <span>*</span> </p> <input type="text" name="title" value="">
+      <p>message <span>*</span> </p> <textarea name="message" rows="8" cols="80"></textarea>
+      <button class='button' type="submit" name="button"　class='button'>送信</button>
+    </form>
+  </body>
+</html>
